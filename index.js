b6dcfea4c00f14cc2508516ce9d5590c68ff01d6
shreeshyla-aither <shreeshyla@aithertechnology.com>
2022-10-13 19:34:22 +0000

    auto-commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var ses = new aws.SES({ region: "us-east-1" });
 exports.handler = async function (event) {
   var params = {
     Destination: {
-      ToAddresses: ["nick@aithertechnology.com"],
+      ToAddresses: ["shree@aithertechnology.com"],
     },
     Message: {
       Body: {
