26b63a4ad1034db9ad82d698e6c9fb958943f0a7
shreeshyla-aither <shreeshyla@aithertechnology.com>
2022-10-13 15:48:41 +0000

    auto-commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-INITIAL FILE
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "us-east-1" });
+exports.handler = async function (event) {
+  var params = {
+    Destination: {
+      ToAddresses: ["nick@aithertechnology.com"],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "Test" },
+      },
+
+      Subject: { Data: "Test Email" },
+    },
+    ReplyToAddresses: ["123456@cforia.aithertechnology.com"],
+    ReturnPath: "return_123456@cforia.aithertechnology.com",
+    Source: "noreply@aithertechnology.com",
+  };
+ 
+  return ses.sendEmail(params).promise()
+};
